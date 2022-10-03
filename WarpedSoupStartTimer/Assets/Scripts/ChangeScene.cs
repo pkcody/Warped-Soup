dcd767e25579f31c8dac784e3dacc92882a71fe8
calvinsoliee <77906888+calvinsoliee@users.noreply.github.com>
2022-10-02 21:08:07 -0700

    Added Timer Start and End Button and Camera Change

---
+++ WarpedSoupStartTimer/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,14 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public CountdownTimer countdownTimer;
+    public GameObject cam;
+
+    private void Update()
+    {
+        countdownTimer = GetComponent<currentTime>();
+    }
+}*/
---
+++ WarpedSoupStartTimer/Assets/Scripts/Menu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+}
